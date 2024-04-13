872cfd7267325b4faa684fc1400f9680b70d71dd
hyunw9 <rkdgusdnr32@naver.com>
2024-04-13 16:52:52 +0900

    feat: Security Config 추가

---
+++ src/main/java/com/mju/capstone/global/security/util/SecurityUtil.java
@@ -0,0 +1,23 @@
+package com.mju.capstone.global.security.util;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SecurityUtil {
+
+  private SecurityUtil(){
+  }
+
+  public static Long getCurrentMemberId(){
+    //SecurityContext의 Authentication 을 이용해 회원 정보 가져오기
+
+    final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    if(authentication == null || authentication.getName()==null ){
+      throw new RuntimeException("Security Context에 인증 정보가 없습니다. ");
+    }
+
+    return Long.parseLong(authentication.getName());
+  }
+
+}
