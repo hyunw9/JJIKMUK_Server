385926905c6d94304e728f47ce2273192dcab49a
hyunw9 <rkdgusdnr32@naver.com>
2024-04-15 18:26:53 +0900

    feat : 통일 Body 적용

--- src/main/java/com/mju/capstone/auth/controller/AuthController.java
+++ src/main/java/com/mju/capstone/auth/controller/AuthController.java
@@ -6,7 +6,9 @@ import com.mju.capstone.auth.dto.response.MemberRes;
 import com.mju.capstone.auth.service.AuthService;
 import com.mju.capstone.auth.token.dto.TokenDto;
 import com.mju.capstone.auth.token.dto.TokenRequest;
+import com.mju.capstone.global.response.ControllerMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,17 +23,17 @@ public class AuthController {
   private final AuthService authService;
 
   @PostMapping("/signup")
-  public ResponseEntity<MemberRes> signup(@RequestBody MemberReq memberReq){
-    return ResponseEntity.ok(authService.signup(memberReq));
+  public ResponseEntity<?> signup(@RequestBody MemberReq memberReq){
+    return new ResponseEntity<>(new ControllerMessage(HttpStatus.CREATED,"성공", authService.signup(memberReq)),HttpStatus.CREATED);
   }
 
   @PostMapping("/login")
-  public ResponseEntity<TokenDto> login(@RequestBody LoginReq loginReq){
-    return ResponseEntity.ok(authService.login(loginReq));
+  public ResponseEntity<?> login(@RequestBody LoginReq loginReq){
+    return new ResponseEntity<>(new ControllerMessage(HttpStatus.OK,"성공",authService.login(loginReq)),HttpStatus.OK);
   }
 
   @PostMapping("/reissue")
-  public ResponseEntity<TokenDto> reissue(@RequestBody TokenRequest tokenRequest){
-    return ResponseEntity.ok(authService.reissue(tokenRequest));
+  public ResponseEntity<?> reissue(@RequestBody TokenRequest tokenRequest){
+    return new ResponseEntity<>(new ControllerMessage(HttpStatus.OK,"성공",authService.reissue(tokenRequest)),HttpStatus.OK);
   }
 }
