cd2c678aeb579043d41a36221fbb0785baa97cdf
hyunw9 <rkdgusdnr32@naver.com>
2024-04-17 18:55:40 +0900

    feat : Custom Error Handler 추가

--- src/main/java/com/mju/capstone/auth/exception/JwtAccessDeniedHandler.java
+++ src/main/java/com/mju/capstone/auth/exception/JwtAccessDeniedHandler.java
@@ -1,19 +1,35 @@
 package com.mju.capstone.auth.exception;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mju.capstone.global.response.ControllerMessage;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStream;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
+  private static ControllerMessage exceptionMessage ;
+
   @Override
   public void handle(HttpServletRequest request, HttpServletResponse response,
       AccessDeniedException accessDeniedException) throws IOException, ServletException {
-    //필요 권한 없이 접근하려 할 때 Handler
-    response
-        .sendError(HttpServletResponse.SC_FORBIDDEN);
+
+    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+    exceptionMessage = ControllerMessage.builder()
+        .status(HttpStatus.FORBIDDEN)
+        .message("잘못된 접근입니다.")
+        .build();
+    try(OutputStream os = response.getOutputStream()){
+      ObjectMapper objectMapper = new ObjectMapper();
+      objectMapper.writeValue(os,exceptionMessage);
+      os.flush();
+    }
   }
 }
--- src/main/java/com/mju/capstone/auth/exception/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/mju/capstone/auth/exception/JwtAuthenticationEntryPoint.java
@@ -1,10 +1,15 @@
 package com.mju.capstone.auth.exception;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mju.capstone.global.response.ControllerMessage;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStream;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -12,11 +17,22 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
-
+  private static  ControllerMessage exceptionDto ;
   @Override
   public void commence(HttpServletRequest request, HttpServletResponse response,
       AuthenticationException authException) throws IOException, ServletException {
-    response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-    log.error("Unauthorized error : "+ authException);
+
+    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    exceptionDto = ControllerMessage.builder()
+        .status(HttpStatus.UNAUTHORIZED)
+        .message("비정상적인 토큰입니다.")
+        .build();
+
+    try(OutputStream os = response.getOutputStream()){
+      ObjectMapper objectMapper = new ObjectMapper();
+      objectMapper.writeValue(os,exceptionDto);
+      os.flush();
+    }
   }
 }
