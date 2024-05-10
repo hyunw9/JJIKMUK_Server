3a31ed5c93dd638afa81a4f0748c17bedf3f4a8f
hyunw9 <rkdgusdnr32@naver.com>
2024-05-10 14:32:49 +0900

    [feat/#32] : date request 타입 변경 : header -> param

--- src/main/java/com/mju/capstone/food/controller/ItemController.java
+++ src/main/java/com/mju/capstone/food/controller/ItemController.java
@@ -5,6 +5,7 @@ import com.mju.capstone.food.service.ItemService;
 import com.mju.capstone.global.response.ControllerMessage;
 import com.mju.capstone.global.response.message.SuccessMessage;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
@@ -14,8 +15,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -43,7 +44,8 @@ public class ItemController {
       description = "LocalDate 형식으로 유저가 해당 일에 어떤 음식을 섭취했는지 조회합니다. "
   )
   public ResponseEntity<?> getItemByLocalDate(
-      @RequestHeader @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date
+      @Parameter(required = true, example = "2024-05-08")
+      @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date
   ) {
     return ResponseEntity.status(HttpStatus.OK)
         .body(ControllerMessage.of(SuccessMessage.OK, itemService.findItemHistoryByDate(date)));
