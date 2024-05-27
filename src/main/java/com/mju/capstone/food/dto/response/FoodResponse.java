dd3cd144b47d56b3ee9ae7098565c9820f23bdec
hyunw9 <rkdgusdnr32@naver.com>
2024-05-27 21:40:29 +0900

    [refactor/#40] : 엔티티 추가 정보 반환

--- src/main/java/com/mju/capstone/food/dto/response/FoodResponse.java
+++ src/main/java/com/mju/capstone/food/dto/response/FoodResponse.java
@@ -1,16 +1,21 @@
 package com.mju.capstone.food.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record FoodResponse(
     String name,
     int kcal,
     int carbohydrate,
     int protein,
     int fat,
-    String imgUrl
+    String imgUrl,
+    int amount,
+    int serving
 ) {
 
-  public static FoodResponse of(String name, int kcal, int carbohydrate, int protein, int fat, String imgUrl) {
-    return new FoodResponse(name, kcal, carbohydrate, protein, fat, imgUrl);
+  public static FoodResponse of(String name, int kcal, int carbohydrate, int protein, int fat,
+      String imgUrl, int amount, int serving) {
+    return new FoodResponse(name, kcal, carbohydrate, protein, fat, imgUrl, amount,serving);
   }
-
 }
