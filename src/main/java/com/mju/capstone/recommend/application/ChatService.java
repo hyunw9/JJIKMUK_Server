4591a940ade62d860a097ed33c8d06e9daedb5e8
hyunw9 <rkdgusdnr32@naver.com>
2024-05-30 18:53:26 +0900

    [feat/#44] : 추천 필터 추가 & 유저 히스토리 반영하도록 변경

--- src/main/java/com/mju/capstone/recommend/application/ChatService.java
+++ src/main/java/com/mju/capstone/recommend/application/ChatService.java
@@ -1,12 +1,14 @@
 package com.mju.capstone.recommend.application;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mju.capstone.food.entity.Food;
 import com.mju.capstone.food.service.StaticFoodService;
-import com.mju.capstone.recommend.config.AppConfig;
+import com.mju.capstone.member.dto.response.GoalNutritionResponse;
+import com.mju.capstone.member.service.GoalService;
+import com.mju.capstone.member.service.MemberService;
 import com.mju.capstone.recommend.domain.GptManager;
 import com.mju.capstone.recommend.dto.request.MenuRecommendRequest;
 import com.mju.capstone.recommend.dto.response.RecommendResponse;
+import com.mju.capstone.recommend.dto.response.SupposedNutrition;
 import com.mju.capstone.recommend.dto.response.TotalRecommendResponse;
 import com.mju.capstone.recommend.repository.FoodRepositoryImpl;
 import lombok.RequiredArgsConstructor;
@@ -19,28 +21,39 @@ import org.springframework.stereotype.Service;
 public class ChatService {
 
   private final GptManager gptManager;
-  private final AppConfig appConfig;
-  private final ObjectMapper objectMapper;
   private final StaticFoodService staticFoodService;
   private final FoodRepositoryImpl foodRepository;
+  private final MemberService memberService;
+  private final GoalService goalService;
 
   public TotalRecommendResponse getChatResponse(MenuRecommendRequest menuRecommendRequest) {
-    StringBuilder sb = new StringBuilder();
-    sb.append("업로드한 파일 내에서 ").append(menuRecommendRequest.cookOrDelivery()).append("로 ");
-    sb.append(menuRecommendRequest.mealTime()).append("에 먹을 ")
-        .append(menuRecommendRequest.tasteType()).append("한 ").append(menuRecommendRequest.menuCountry())
-        .append("을 추천해줘.\n")
-        .append("답은 1개 여야하고, 다양하게 추천해줘. 응답 형식: JSON  { name: String }");
 
-    RecommendResponse response = gptManager.sendOpenAIRequest(sb.toString());
-    log.info(sb.toString());
+    GoalNutritionResponse memberGoal = goalService.getMemberGoal();
+    SupposedNutrition supposedNutrition = goalService.calculateSupposedNutrition(memberGoal);
+    SupposedNutrition calculatedSupposedNutrition = goalService.calculateMealTimeRate(
+        menuRecommendRequest.mealTime(), supposedNutrition);
+
+    String prompt = createNutritionPrompt(menuRecommendRequest, calculatedSupposedNutrition);
+    RecommendResponse response = gptManager.sendOpenAIRequest(prompt);
+    log.info(prompt);
     return getTotalRecommendResponseByFoodName(response);
   }
 
-  public TotalRecommendResponse getTotalRecommendResponseByFoodName(RecommendResponse response){
+  private String createNutritionPrompt(MenuRecommendRequest request,
+      SupposedNutrition supposedNutrition) {
+    return String.format(
+        "사용자가 %s에 %s로 먹을 재료가 %s이고 영양소가 %d kcal, %d carbohydrate, %d protein, %d fat 와 근접한 음식을 " +
+            "업로드한 파일 내에서 추천해줘. 응답 형식은 다른 말 없이 무조건 다음과 같아야 해 : JSON  { name: String }",
+        request.mealTime(), request.cookOrDelivery(), request.ingredient(), supposedNutrition.kcal()
+        , supposedNutrition.carbohydrate(), supposedNutrition.protein(), supposedNutrition.fat()
+    );
+  }
+
+  public TotalRecommendResponse getTotalRecommendResponseByFoodName(RecommendResponse response) {
     Food food = foodRepository.findByName(response.name());
 
-    return TotalRecommendResponse.of(food.getName(),food.getKcal(),
-        food.getCarbohydrate(), food.getProtein(), food.getFat(), food.getImgUrl(),food.getAmount(),food.getServing());
+    return TotalRecommendResponse.of(food.getName(), food.getKcal(),
+        food.getCarbohydrate(), food.getProtein(), food.getFat(), food.getImgUrl(),
+        food.getAmount(), food.getServing());
   }
 }
