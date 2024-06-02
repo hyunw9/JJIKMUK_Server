137c99c782f1ad837dca347db94dca5a4aab487d
hyunw9 <rkdgusdnr32@naver.com>
2024-06-02 22:34:48 +0900

    [chore/#56] : 이름 형식 등 수정

--- src/main/java/com/mju/capstone/member/service/calculator/InitialCalorieCalculatorFactory.java
+++ src/main/java/com/mju/capstone/member/service/calculator/InitialCalorieCalculatorFactory.java
@@ -1,32 +1,33 @@
 package com.mju.capstone.member.service.calculator;
 
-import com.mju.capstone.auth.dto.request.MemberReq;
+import com.mju.capstone.member.dto.request.NutritionCalculatorRequest;
 import com.mju.capstone.member.entity.Gender;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class InitialCalorieCalculatorFactory {
-  public static CalorieCalculator getCalorieCalculator(MemberReq memberReq){
-    if(memberReq.gender().equals(Gender.MALE)){
+  public static CalorieCalculator getCalorieCalculator(
+      NutritionCalculatorRequest request){
+    if(request.gender().equals(Gender.MALE)){
       log.info("MALE Calculator 생성");
       return MaleCalculator.builder()
-          .birth(memberReq.birth())
-          .height(memberReq.height())
-          .weight(memberReq.weight())
-          .level(memberReq.level())
-          .dietPlan(memberReq.dietPlan())
+          .birth(request.birth())
+          .height(request.height())
+          .weight(request.weight())
+          .level(request.level())
+          .dietPlan(request.dietPlan())
           .build();
-    }else if(memberReq.gender().equals(Gender.FEMALE)){
+    }else if(request.gender().equals(Gender.FEMALE)){
       log.info("FEMALE Calculator 생성");
       return FemaleCalculator.builder()
-          .birth(memberReq.birth())
-          .height(memberReq.height())
-          .weight(memberReq.weight())
-          .level(memberReq.level())
-          .dietPlan(memberReq.dietPlan())
+          .birth(request.birth())
+          .height(request.height())
+          .weight(request.weight())
+          .level(request.level())
+          .dietPlan(request.dietPlan())
           .build();
     }else{
-      log.error(memberReq.gender()+"");
+      log.error(request.gender()+"");
       throw new IllegalArgumentException("잘못된 성별입니다.");
     }
   }
